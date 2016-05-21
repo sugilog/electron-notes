b3ba171895cc58ded8fea610fc0e98f48ca81e73
sugilog <sugilog@gmail.com>
2016-05-21 14:28:28 +0900

    available to choose directory by clicking anchor

--- renderer.js
+++ renderer.js
@@ -1,27 +1,37 @@
 const workspace = require( "./workspace.js" );
 
+function listEntries() {
+  workspace.openDir( function( err, dir ) {
+    if ( err ) {
+      console.log( err );
+    }
+    else {
+      workspace.list( dir, function( err, entry ) {
+        if ( err ) {
+          console.log( err );
+        }
+        else {
+          console.log( entry );
+        }
+      });
+    }
+  });
+}
+
 exports.ready = function( global ) {
   global.addEventListener( "DOMContentLoaded", function( event ) {
-    console.log( "DOMContentLoaded", event );
-
-    workspace.openDir( function( err, dir ) {
-      if ( err ) {
-        console.log( err );
-      }
-      else {
-        workspace.list( dir, function( err, entry ) {
-          if ( err ) {
-            console.log( err );
-          }
-          else {
-            console.log( entry );
-          }
-        });
-      }
-    });
+    listEntries();
   });
 
   global.addEventListener( "hashchange", function( event ) {
     console.log( "hashchang", event )
+
+    if ( "#!" === global.location.hash ) {
+      // do nothing
+    }
+    else if ( "#!/open-dir" === global.location.hash ) {
+      listEntries();
+      global.location.hash = "#!";
+    }
   });
 }
