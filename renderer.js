71813605a30a7ceaeeb35425a9540ffe57ea06c6
sugilog <sugilog@gmail.com>
2016-05-21 14:03:31 +0900

    filelistを取得してみる

--- renderer.js
+++ renderer.js
@@ -1,6 +1,17 @@
+const workspace = require( "./workspace.js" );
+
 exports.ready = function( global ) {
   global.addEventListener( "DOMContentLoaded", function( event ) {
     console.log( "DOMContentLoaded", event );
+
+    workspace.list( function( err, entry ) {
+      if ( err ) {
+        console.log( err );
+      }
+      else {
+        console.log( entry );
+      }
+    });
   });
 
   global.addEventListener( "hashchange", function( event ) {
---
+++ workspace.js
@@ -0,0 +1,42 @@
+const fs = require( "fs" ),
+      path = require( "path" );
+
+function entryType( stat ) {
+  if ( stat.isFile() ) {
+    return "file";
+  }
+  else if ( stat.isDirectory ) {
+    return "directory";
+  }
+  else {
+    return "unsupported";
+  }
+}
+
+function list( callback ) {
+  const dir = path.resolve( path.resolve( __dirname ) );
+
+  fs.readdir( dir, function( err, entries ) {
+    if ( err ) {
+      callback( err );
+    }
+    else {
+      entries.forEach( function( entry ) {
+        var entrypath = path.resolve( dir, entry ),
+            stat      = fs.statSync( entrypath );
+
+        callback( null, {
+          name:      entry,
+          path:      entrypath,
+          size:      stat.size,
+          updatedAt: stat.mtime,
+          type:      entryType( stat )
+        });
+      });
+    }
+  });
+}
+
+module.exports = {
+  list: list
+}
