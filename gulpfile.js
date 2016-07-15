438645f54cde15a96fdaf2a1ea18e1ebb3406ecd
sugilog <sugilog@gmail.com>
2016-07-15 18:43:55 +0900

    separate source file and babel/copy files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp  = require( "gulp" ),
     babel = require( "gulp-babel" );
 
 gulp.task( "babel", function() {
-  gulp.src( "./**/*.jsx" )
+  gulp.src( "./lib/**/*.jsx" )
     .pipe( babel() )
-    .pipe( gulp.dest( "./" ) );
+    .pipe( gulp.dest( "./app/" ) );
 });
 
 gulp.task( "watch", function() {
-  gulp.watch( "./**/*.jsx", [ "babel" ] );
+  gulp.watch( "./lib/**/*.jsx", [ "babel" ] );
 });
 
 gulp.task( "default", [ "babel", "watch" ] );
